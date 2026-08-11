f10f9d90b18f13e9c7dd9a6d7abc28934f822a56
varun <varun4socials@gmail.com>
2026-08-11 17:35:11 +0530

    Attach bot method to send images with captions

--- internal/telegram/methods.go
+++ internal/telegram/methods.go
@@ -5,7 +5,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"mime/multipart"
 	"net/http"
+	"strconv"
 )
 
 func (bot *TelegramBot) sendJSON(endpoint string, payload any) error {
@@ -46,4 +48,54 @@ func (bot *TelegramBot) deleteMessage(chatId int64, messageId int) error {
 	})
 
 	return err
+}
+
+func (bot *TelegramBot) sendPhoto(chatId int64, imageBytes []byte, caption string) error {
+	var requestBody bytes.Buffer
+
+	writer := multipart.NewWriter(&requestBody)
+
+	part, err := writer.CreateFormFile("photo", "image.jpg")
+	if err != nil {
+		return fmt.Errorf("UNABLE TO CREATE FORM FILE : %w", err)
+	}
+
+	if _, err := part.Write(imageBytes); err != nil {
+		return fmt.Errorf("UNABLE TO WRITE IMAGE BYTES : %w", err)
+	}
+
+	if err := writer.WriteField("chat_id", strconv.FormatInt(chatId, 10)); err != nil {
+		return fmt.Errorf("UNABLE TO WRITE CHAT ID : %w", err)
+	}
+
+	if caption != "" {
+		if err := writer.WriteField("caption", caption); err != nil {
+			return fmt.Errorf("UNABLE TO WRITE CAPTION : %w", err)
+		}
+	}
+
+	if err := writer.Close(); err != nil {
+		return fmt.Errorf("UNABLE TO CLOSE WRITER : %w", err)
+	}
+
+	endpoint := bot.API_URL + "sendPhoto"
+	req, err := http.NewRequest("POST", endpoint, &requestBody)
+	if err != nil {
+		return fmt.Errorf("UNABLE TO CREATE REQUEST : %w", err)
+	}
+
+	req.Header.Set("Content-Type", writer.FormDataContentType())
+
+	resp, err := bot.Client.Do(req)
+	if err != nil {
+		return fmt.Errorf("UNABLE TO SEND REQUEST : %w", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		bodyBytes, _ := io.ReadAll(resp.Body)
+		return fmt.Errorf("TELEGRAM API Error %d: %s", resp.StatusCode, string(bodyBytes))
+	}
+
+	return nil
 }
